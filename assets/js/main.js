c948995f5658d184746dd94066db088152af2f1e
Diogn <rino.dwicahyo@gmail.com>
2018-07-11 08:38:57 +0700

    adding hover style to nav function

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,25 @@
 $(window).scroll(function(){
   $('nav').toggleClass('scrolled', $(this).scrollTop() > 450);
+  $('dropdown-menu').toggleClass('changed', $(this).scrollTop() > 450);
+  $('dropdown-menu').toggleClass('scrolled', $(this).scrollTop() > 450);
 });
+
+$(window).close(function(){
+  $('dropdown-menu').toggleClass('')
+})
+
+// $(document).ready(function() {
+//   var scroll_start = 0;
+//   var startchange = $('.dropdown-menu').click();
+//   var offset = startchange.offset();
+//   if (startchange.length) {
+//     $(document).scroll(function(){
+//       scroll_start = $(this).scrollTop();
+//       if(scroll_start > offset.top ) {
+//         $(".dropdown-menu").css('background-color', 'grey');
+//       } else {
+//         $(".navbar-default").css('background-color', 'transparent');
+//       }
+//     });
+//   }
+// });
