1b424c9b30cc504dd59de6f31248963c897eea26
Diogn <rino.dwicahyo@gmail.com>
2018-07-12 19:50:12 +0700

    decrease font weight, and still strugling with responsive navbar with block display

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,10 @@
+// Navbar Bg-color changed when scrolled down
 $(window).scroll(function(){
   $('nav').toggleClass('scrolled', $(this).scrollTop() > 450);
 });
+
+window.setTimeout(function(){
+  $(".navbar-collapse").fadeTo(500,0).slideUp(500, function() {
+    $(this).remove();
+  });
+}, 2000);
